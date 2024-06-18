595c4b294cc9c604ace36eac1eb45744e5c2fae3
immeo <wayfarer245@gmail.com>
2024-06-18 18:50:55 +0300

    06-add search logic

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,6 +7,11 @@ export class Search extends DivComponent {
 		this.state = state;
 	}
 
+	search() {
+		const searchInputValue = this.el.querySelector('.search__book_input').value;
+		this.state.searchQuery = searchInputValue;
+	}
+
 	render() {
 		this.el.classList.add('search');
 		this.el.innerHTML = `
@@ -22,6 +27,16 @@ export class Search extends DivComponent {
             </div>
             <button aria-label="search"><img src="/static/search-white.svg" alt="Seacrch icon" /></button>
 		`;
+		this.el
+			.querySelector('button')
+			.addEventListener('click', this.search.bind(this));
+		this.el
+			.querySelector('.search__book_input')
+			.addEventListener('keydown', e => {
+				if (e.key === 'Enter') {
+					this.search();
+				}
+			});
 		return this.el;
 	}
 }
